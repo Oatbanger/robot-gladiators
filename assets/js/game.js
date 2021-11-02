5c0f1c3bff64b3d36a85cca62e09b0419d440b5d
Cameron Nord <cnord6@gmail.com>
2021-11-02 01:49:31 -0600

    start of control flow

--- assets/js/game.js
+++ assets/js/game.js
@@ -12,25 +12,16 @@ var enemyAttack = 12;
 var fight = function() {
     // Alert players that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
+    var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' OR 'SKIP' to choose.");
 
-    //Subtract the value of 'PlayerAttack' from the value of 'enemyHealth' and use that result to upate the value in the 'enemyHealth' variable
-    enemyHealth = enemyHealth - playerAttack;
+    if (promptFight === "fight" || promptFight === "FIGHT") {
+        // REMOVE ENERMY'S HEALTH BY SUBTRACTING THE AMOUNT SET IN THE playerAttack variable
+        enemyHealth = enemyHealth - playerAttack;
+        console.log(
+            playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining. "
+        );
 
-    // Log a resulting message to the console so we know that it worked.
-
-    console.log(
-        playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining. "
-    );
-
-    // ckeck enemy's health
-    if (enemyHealth <= 0) {
-        window.alert(enemyName + " has died!");
-    }
-    else {
-        window.alert(enemyName + " still has " + enemyHealth + " health left.");
-    }
-    
-    // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
+         // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
     playerHealth = playerHealth - enemyAttack;
 
     // check player's health
@@ -46,6 +37,21 @@ var fight = function() {
     console.log(
         enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining. "
     );
+
+
+    
+   
+    }
+
+
+    //Subtract the value of 'PlayerAttack' from the value of 'enemyHealth' and use that result to upate the value in the 'enemyHealth' variable
+    
+
+    // Log a resulting message to the console so we know that it worked.
+
+
+
+ 
 };
 
 fight();
